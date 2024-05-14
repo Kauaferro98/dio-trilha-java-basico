e8f2e27cf2e9435105f2ac5add70163def4e486b
Kauã Ferro <kaua.ferro@icloud.com>
2024-05-14 18:24:32 -0300

    -

--- tipos-variaveis/src/tiposVariaveis.java
+++ tipos-variaveis/src/tiposVariaveis.java
@@ -1,5 +1,10 @@
 public class tiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;
+        System.out.println(salarioMinimo);
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+
     }
 }
