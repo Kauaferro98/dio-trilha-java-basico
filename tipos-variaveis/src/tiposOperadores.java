e082a678af8235a7d817c4aaa0789aacb4f17f36
Kauã Ferro <kaua.ferro@icloud.com>
2024-05-14 18:40:47 -0300

    Operadores

---
+++ tipos-variaveis/src/tiposOperadores.java
@@ -0,0 +1,11 @@
+public class tiposOperadores {
+    public static void main(String[] args) {
+        //classe Operadores.java
+        String nome = "GLEYSON";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        //Date dataNascimento = new Date();
+    }
+}
