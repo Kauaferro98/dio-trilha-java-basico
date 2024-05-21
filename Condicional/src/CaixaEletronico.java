dea6ee0185f0b696a6ea876fd80ad4c557d519b4
Kauã Ferro <kaua.ferro@icloud.com>
2024-05-21 15:37:42 -0300

    condicional simples

--- Condicional/src/CaixaEletronico.java
+++ Condicional/src/CaixaEletronico.java
@@ -1,7 +1,13 @@
 
 public class CaixaEletronico {
     public static void main(String[] args) {
-        System.out.println("Teste");
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
+ 
+        if(valorSolicitado < saldo)
+         saldo = saldo - valorSolicitado;
+ 
+         System.out.println(saldo);
     }
     
 }
