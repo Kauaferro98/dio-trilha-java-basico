e3fa93d38e95bdc00a589506a27f5f7ec9f1a787
Kauã Ferro <kaua.ferro@icloud.com>
2024-05-27 15:49:11 -0300

    Arrays

--- Condicional/src/ExemploFor.java
+++ Condicional/src/ExemploFor.java
@@ -1,8 +1,10 @@
-// ExemploFor.java
 public class ExemploFor {
-	public static void main(String[] args) {
-		for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
-			System.out.println(carneirinhos + " - Carneirinho(s)");
-		}
-	}
-}
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+    
+        for (int x=0; x<alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+    
+    }
+    }
